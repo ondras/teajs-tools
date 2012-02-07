482716be362ea37c4b94bad0f81ad65b4243f443
ondrej.zara@gmail.com <ondrej.zara@gmail.com>
2012-02-07 13:40:52 +0100

    updates for debian

--- export.sh
+++ export.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-VERSION="`cat svn/VERSION`-svn"
 cd svn
+VERSION=$(cat VERSION)
+
+if [[ $VERSION =~ dev ]] ; then
+	VERSION="${VERSION}~svn"$(svnversion)
+fi
+
 svn export --force . ../v8cgi-${VERSION}
 cd ..
 tar -czf v8cgi_${VERSION}.orig.tar.gz v8cgi-${VERSION}
